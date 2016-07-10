78e422b34650c764846be3358436c04741fccac4
John W Kerns <jkerns@packetsar.com>
2016-07-09 22:57:12 -0700

    Fixed attributes and wrote readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # tinyxmltodict
-A small and simple python XML to Dict converter
+A Skinny XML to Python dictionary converter which works in Python2 or Python3 and requires **NO** non-native libraries/modules
+
+
+
+###About
+Tinyxmltodict was built to do XML conversions for simple data structures (like REST calls) whilst being very lightweight, portable, understandable, and requiring as few external dependencies as possible (you don't need to install any Python modules).
+
+It works with simple XML element names and attributes. Some of the more complex XML formats may not work properly.
+
+
+
+###Input, Output
+The only argument "inputdata" should be a string and can be either a file path, or a string of XML data; the method automatically detects which was used (assuming you don't use "<" in your file names)
+
+The output is a formatted python dictionary of the XML data
+
+
+
+###Usage and Examples
+Tinyxmltodict can be imported as a module (if that's how you want to do it), but was written to be easily copied and pasted into your own codebase (fewer files to manage). Use it however you like.
+
+
+#####Direct in Code
+\>>>`tinyxmltodict('/root/somefile.xml')` # Parsing Linux/Unix file directly
+
+\>>>`tinyxmltodict('C:\\Users\\Public\\Desktop\\somefile.xml')` # Parsing Windows file directly (Use double-slashes)
+
+\>>>`tinyxmltodict('''<food><veg>Arugula</veg><veg>Celery</veg><fru>Apple</fru></food>''')` # Parsing direct XML input
+
+
+#####As a Module
+\>>>`import tinyxmltodict as txd` # Import the module
+
+\>>>`txd.tinyxmltodict('/root/somefile.xml')` # Parsing Linux/Unix file directly
+
+\>>>`txd.tinyxmltodict('C:\\Users\\Public\\Desktop\\somefile.xml')` # Parsing Windows file directly (Use double-slashes)
+
+\>>>`txd.tinyxmltodict('''<food><veg>Arugula</veg><veg>Celery</veg><fru>Apple</fru></food>''')` # Parsing direct XML input
--- tinyxmltodict.py
+++ tinyxmltodict.py
@@ -1,40 +1,62 @@
+#!/usr/bin/python
+
+#####       Written by John W Kerns        #####
+#####      http://blog.packetsar.com       #####
+#####    https://github.com/packetsar/     #####
 
 
+##################################### T I N Y   X M L T O D I C T #####################################
+#######################################################################################################
 
-import xml.etree.ElementTree
-import re
+##### Skinny XML to Python dictionary converter which works in Python2 or Python3 and requires NO non-native libraries/modules #####
+##### Works with simple XML element names and attributes. Some XML formats may not work properly #####
+##### Input argument "inputdata" (str) can be either a file path, or a string of XML data; the method detects which was used #####
+##### Output is a formatted python dictionary of the XML data #####
 
-def tinyxml2dict_recurse(node):
-	if len(node.getchildren()) == 0:
-		result = node.text
-	else:
-		result = dict()
-		for child in node:
-			if child.tag not in result.keys():
-				result[child.tag] = tinyxml2dict(child)
-			else:
-				if type(result[child.tag]) != type([]):
-					result[child.tag] = [result[child.tag], tinyxml2dict(child)]
-				else:
-					result[child.tag].append(tinyxml2dict(child))
+import xml.etree.ElementTree # Built in module for parsing the XML elements
+
+##### Self-recursive method for XML conversion #####
+def tinyxmltodict_recurse(node):
+	if len(node.getchildren()) == 0 and len(node.items()) == 0: # If there are no more child elements, and no element attributes
+		result = node.text # Set the element text as the result and return it
+	else: # If there are children and/or attributes
+		result = {} # Start with empty dict
+		if len(node.items()) > 0: # If attributes exist for the element
+			result.update(dict(node.items())) # Add the attributes to the result
+		for child in node: # For each child of this element node
+			if child.tag not in result.keys(): # If this child element does not have an existing key
+				result[child.tag] = tinyxmltodict_recurse(child) # Add the key and iterate again
+			else: # If this child element does have an existing key
+				if type(result[child.tag]) != type([]): # And it is not already a list
+					result[child.tag] = [result[child.tag], tinyxmltodict_recurse(child)] # Nest it in a list and iterate again
+				else: # If it is already a list
+					result[child.tag].append(tinyxmltodict_recurse(child)) # Append to that list
 	return result
 
 ##### Simple starter method for the XML to Dict conversion process #####
-def tinyxml2dict(mode, input):
-	if mode == "file":
-		with open(input, 'r') as filetext:
-			xmldata = filetext.read()
-	elif mode == "string":
-		xmldata = input
-	regex = "(?s)<!--.*-->"
-	configcomment = re.findall(regex, xmldata, flags=0)[0]
-	cleanedxml = xmldata.replace(configcomment, "")
-	root = xml.etree.ElementTree.fromstring(cleanedxml)
-	return {root.tag: tinyxml2dict_recurse(root)}
+def tinyxmltodict(inputdata):
+	if "<" not in inputdata: # If the 'less than' symbol is not in the inputdata, it is likely a file path (don't use "<" in your file names, damnit!)
+		with open(inputdata, 'r') as filetext: # Open file in read mode
+			xmldata = filetext.read() # Suck in text of file
+			filetext.close() # And close the file
+	else: # If "<" is in the inputdata, then you are likely inputting XML data as a string
+		xmldata = inputdata # So set the xmldata variable as your string input
+	root = xml.etree.ElementTree.fromstring(xmldata) # Mount the root element
+	return {root.tag: tinyxmltodict_recurse(root)} # Set root element name as key and start first iteration
 
-
-
-
-
-
-
+########################################## USAGE AND EXAMPLES #########################################
+#
+############ Used natively in your code ############
+#>>> tinyxmltodict('/root/somefile.xml') # Parsing Linux/Unix file directly
+#>>> tinyxmltodict('C:\\Users\\Public\\Desktop\\somefile.xml') # Parsing Windows file directly (Use double-slashes)
+#>>> tinyxmltodict('''<food><veg>Arugula</veg><veg>Celery</veg><fru>Apple</fru></food>''') # Parsing direct XML input
+#
+#
+############ Used as a module ############
+#>>> import tinyxmltodict as txd # Import the module
+#>>> txd.tinyxmltodict('/root/somefile.xml') # Parsing Linux/Unix file directly
+#>>> txd.tinyxmltodict('C:\\Users\\Public\\Desktop\\somefile.xml') # Parsing Windows file directly (Use double-slashes)
+#>>> txd.tinyxmltodict('''<food><veg>Arugula</veg><veg>Celery</veg><fru>Apple</fru></food>''') # Parsing direct XML input
+#
+#######################################################################################################
+#######################################################################################################
