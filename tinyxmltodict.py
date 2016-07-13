a903615923b3b11129c6fb2030340e06ebc2b152
John W Kerns <jkerns@packetsar.com>
2016-07-13 11:54:40 -0700

    Some formatting added

--- tinyxmltodict.py
+++ tinyxmltodict.py
@@ -75,19 +75,34 @@ def tinyxmltodict(inputdata):
 import xml.etree.ElementTree # Built in module for parsing the XML elements
 
 def tinydicttoxml_recurse(node, dictdata):
+	global formatxml,currentindent,indenttext
+	currentindent += 1
 	for element in dictdata:
 		if type(dictdata[element]) == type(""):
 			newnode = xml.etree.ElementTree.SubElement(node, element)
 			newnode.text = dictdata[element]
+			if formatxml:
+				newnode.tail = "\n" + indenttext * currentindent
 		elif type(dictdata[element]) == type({}):
 			newnode = xml.etree.ElementTree.SubElement(node, element)
+			if formatxml:
+				newnode.tail = "\n" + indenttext * currentindent
 			xml.etree.ElementTree.SubElement(newnode, tinydicttoxml_recurse(newnode, dictdata[element]))
 		elif type(dictdata[element]) == type([]):
 			for entry in dictdata[element]:
 				newnode = xml.etree.ElementTree.SubElement(node, element)
+				if formatxml:
+					newnode.tail = "\n" + indenttext * currentindent
 				xml.etree.ElementTree.SubElement(newnode, tinydicttoxml_recurse(newnode, entry))
+	currentindent -= 1
 
 def tinydicttoxml(dictdata):
+	#####Set XML formatting info #####
+	global formatxml,currentindent,indenttext
+	formatxml = True
+	currentindent = 0
+	indenttext = "\t"
+	##################################
 	for key in dictdata:
 		currentroot = xml.etree.ElementTree.Element(key)
 		tinydicttoxml_recurse(currentroot, dictdata[key])
